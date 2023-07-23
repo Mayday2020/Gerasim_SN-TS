48ab27b503913432f246fa804c2aa30100466d9a
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2023-07-23 22:39:02 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li><a href={'#'}>Home</a></li>
+                <li><a href={'#'}>News</a></li>
+                <li><a href={'#'}>Messages</a></li>
+                <li><a href={'#'}>Settings</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
